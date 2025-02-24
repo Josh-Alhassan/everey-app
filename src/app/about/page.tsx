aae861d61d85c4b3448261fb660f5a45badf5a60
Josh-Alhassan <saintjosh26@gmail.com>
2025-02-25 00:22:56 +0100

    add about  article section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,8 @@ import AboutHeader from "@/sections/AboutHeader/AboutHeader";
 import SetUsApart from "@/sections/SetUsApart/SetUsApart";
 import Statement from "@/sections/Statements/Statement";
 import SuccessNumbers from "@/sections/SectionNumbers/SuccessNumbers";
+import Footer from "@/component/Footer/Footer";
+import AboutArticle from "@/sections/AboutArticle/AboutArticle";
 
 const AboutPage: React.FC = () => {
   return (
@@ -10,7 +12,9 @@ const AboutPage: React.FC = () => {
       <AboutHeader />
       <SetUsApart />
       <Statement />
+      <AboutArticle />
       <SuccessNumbers />
+      <Footer />
     </>
   );
 };
