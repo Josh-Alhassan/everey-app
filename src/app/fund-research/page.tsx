3a082a1ff0d889b63a2ed5d0e7822dc74199789f
Josh-Alhassan <saintjosh26@gmail.com>
2025-02-26 23:48:25 +0100

    add fund research header

---
+++ src/app/fund-research/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styleFund from "./FundResearch.module.css";
+import FundResearchHeader from "@/sections/FundResearchHeader/FundResearchHeader";
+
+const FundResearch = () => {
+  return (
+    <div>
+      <FundResearchHeader />
+    </div>
+  );
+};
+
+export default FundResearch;
---
+++ src/sections/FundResearchHeader/FundResearchHeader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./FundResearchHeader.module.css";
+
+const FundResearchHeader = () => {
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.headerTitle}>Fund a Research</h1>
+    </div>
+  );
+};
+
+export default FundResearchHeader;
